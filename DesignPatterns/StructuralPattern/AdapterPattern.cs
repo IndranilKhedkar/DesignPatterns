33430683537f5b0b0e792a04514710b86cc6d358
Indranil Khedkar <Indranil_Khedkar@symantec.com>
2020-04-10 10:54:27 +0530

    Structural Design Patterns: AdapterPattern.cs

---
+++ DesignPatterns/StructuralPattern/AdapterPattern.cs
@@ -0,0 +1,50 @@
+﻿namespace StructuralPatterns
+{
+    public class AdapterPattern
+    {
+        public void Execute()
+        {
+            HotAirBalloon hotAirBalloon = new HotAirBalloon();
+            Adapter adapter = new Adapter(hotAirBalloon);
+            adapter.Fly();
+        }
+
+        private interface IAircraft
+        {
+            public void Fly();
+        }
+
+        private class HotAirBalloon
+        {
+            private readonly string _gasUsed = "Helium";
+
+            public void Fly(string gasUsed)
+            {
+                // Take-off sequence based on the kind of feul
+                // Followed by more code.  
+            }
+
+            // Function returns the gas used by the balloon for flight
+            public string InflateWithGas()
+            {
+                return _gasUsed;
+            }
+        }
+
+        private class Adapter : IAircraft
+        {
+            private readonly HotAirBalloon _hotAirBallon;
+
+            public Adapter(HotAirBalloon hotAirBalloon)
+            {
+                _hotAirBallon = hotAirBalloon;
+            }
+
+            public void Fly()
+            {
+                var fuelUsed = _hotAirBallon.InflateWithGas();
+                _hotAirBallon.Fly(fuelUsed);
+            }
+        }
+    }
+}
